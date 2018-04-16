dd99ab8435542c5aacf4d5ab478ace831dc58298
Sumit Singhal <sumitsinghalniec@gmail.com>
2018-04-16 10:51:38 +0100

    initial Commit

---
+++ src/main/java/com/example/MyGitProject/MyGitProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.MyGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitProjectApplication.class, args);
+	}
+}
